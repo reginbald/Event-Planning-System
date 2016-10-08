e2d7edb5abe54bf504c44654c90c103240bb7902
Jon Reginbald Ivarsson <mail@reginbald.com>
2016-10-08 17:42:47 +0200

    port revert

--- server/app.ts
+++ server/app.ts
@@ -13,8 +13,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.listen(8080, () => {
-    console.log("Server listening on port 8080");
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
 });
 
 export var App = app;
