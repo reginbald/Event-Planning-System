24dc8a70aaba117c5da5062bd702c30b36f11f81
Jón Reginbald Ívarsson <mail@reginbald.com>
2016-10-14 14:43:14 +0200

    employee table update

--- server/models/employee.ts
+++ server/models/employee.ts
@@ -3,9 +3,11 @@ import * as Sequelize from "sequelize";
 
 export interface EmployeeAttribute {
     id?:string;
+    username?:string;
+    password?:string;
+    access?:number;
     name?:string;
     email?:string;
-    password?:string;
 }
 
 export interface EmployeeInstance extends Sequelize.Instance<EmployeeAttribute>, EmployeeAttribute {
@@ -14,3 +16,46 @@ export interface EmployeeInstance extends Sequelize.Instance<EmployeeAttribute>,
 }
 
 export interface EmployeeModel extends Sequelize.Model<EmployeeInstance, EmployeeAttribute> { }
+
+export class EmployeeTable implements Sequelize.DefineAttributes {
+  id:any;
+  username:any;
+  password:any;
+  access:any;
+  name:any;
+  email:any;
+  [name : string] : string;
+  
+
+  constructor() {
+    this.id = {
+      "type": Sequelize.UUID,
+      "allowNull": false,
+      "primaryKey": true
+    }
+    this.username = {
+      "type": Sequelize.STRING(128),
+      "allowNull": false
+    }
+    this.password = {
+      "type": Sequelize.STRING(128),
+      "allowNull": false
+    }
+    this.access = {
+      "type": Sequelize.INTEGER,
+      "allowNull": false
+    }
+    this.name = {
+      "type": Sequelize.STRING(128),
+      "allowNull": false
+    }
+    this.email = {
+      "type": Sequelize.STRING(128),
+      "allowNull": false,
+      "unique": true,
+      "validate": {
+        "isEmail": true
+      }
+    }
+  }
+}
