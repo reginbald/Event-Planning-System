08b969ec80761f900a0230de5184dd047733cc27
einarlogi <einarlogi14@gmail.com>
2016-10-15 19:48:20 +0200

    small fix for heroku #2

--- client/redux/actions/clientActions.js
+++ client/redux/actions/clientActions.js
@@ -1,4 +1,4 @@
-import mockClientApi from '../../api/MockClientApi';
+import mockClientApi from '../../api/mockClientApi';
 import * as types from './actionTypes';
 
 export function getAllClientsSuccess(clients) {
